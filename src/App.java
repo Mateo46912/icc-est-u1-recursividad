b6a1e57c8bb04e5cb08f44f43318176a978c1950
Mateo Cordero <mcorderoc1@est.ups.edu.ec>
2025-04-09 16:55:53 -0500

    Clase recursividad 2

--- src/App.java
+++ src/App.java
@@ -1,22 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int n = 5;
-        int resultadoFinal = factorial(n);
-        System.out.println("Resultado: "+ resultadoFinal);
+    //     int n = 5;
+    //     int resultadoFinal = factorial(n);
+    //     System.out.println("Resultado: "+ resultadoFinal);
 
-    }
+    // }
 
-    public static int factorial(int n){
-        if (n== 0 ){
-            System.out.println("Alcanzé el caso base ");
-            return 1;
-        }
+    // public static int factorial(int n){
+    //     if (n== 0 ){
+    //         System.out.println("Alcanzé el caso base ");
+    //         return 1;
+    //     }
 
-        int resultadoParcial = n * factorial(n-1);
-        System.out.println("Calculando factorial de: "+ n + " * factorial(" + (n-1) + "-1) " );
-        return resultadoParcial;
+    //     int resultadoParcial = n * factorial(n-1);
+    //     System.out.println("Calculando factorial de: "+ n + " * factorial(" + (n-1) + "-1) " );
+    //     return resultadoParcial;
         
+
+        EjerciciosRecursivos ejercicios = new EjerciciosRecursivos();
+        int resultado = ejercicios.fibonacci(5);
+        System.out.println("El Fibonacci de 5 es: " + resultado);
+
+        
+
 
     }
 }
---
+++ src/EjerciciosRecursivos.java
@@ -0,0 +1,21 @@
+public class EjerciciosRecursivos {
+    
+
+    public int fibonacci(int n){
+
+        if(n<=1){
+            return n;
+
+    } 
+
+    return fibonacci (n-1) + fibonacci(n-2);
+}
+    public int sumaConsecutivos(int n ){
+        if(n==1) return 1;
+        return n + sumaConsecutivos(n-1);
+        
+    } 
+
+    }
+
+    
