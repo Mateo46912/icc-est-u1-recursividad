cf4efd340a57de47089e824afbcd5bc243e856e2
Mateo Cordero <mcorderoc1@est.ups.edu.ec>
2025-04-09 16:13:39 -0500

    Recursividad acavada

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int n = 5;
+        int resultadoFinal = factorial(n);
+        System.out.println("Resultado: "+ resultadoFinal);
+
+    }
+
+    public static int factorial(int n){
+        if (n== 0 ){
+            System.out.println("Alcanzé el caso base ");
+            return 1;
+        }
+
+        int resultadoParcial = n * factorial(n-1);
+        System.out.println("Calculando factorial de: "+ n + " * factorial(" + (n-1) + "-1) " );
+        return resultadoParcial;
+        
+
+    }
+}
